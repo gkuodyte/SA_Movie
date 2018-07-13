dfa1ea4860833a83a2ac2f9cc85c887272fed841
jordanm84 <jordan.marshall@northumbria.ac.uk>
2018-07-13 18:26:17 +0100

    Setting up

--- Application/src/app/app.module.ts
+++ Application/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DetailsComponent } from './details/details.component';
 import { LandingComponent } from './landing/landing.component';
 import { MoviePromoComponent } from './movie-promo/movie-promo.component';
+
+const appRoutes: Routes =
+[
+  {path: '', component: LandingComponent},
+  {path: 'test', component: MoviePromoComponent},
+  {path: 'details', component: DetailsComponent},
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +22,9 @@ import { MoviePromoComponent } from './movie-promo/movie-promo.component';
     MoviePromoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
