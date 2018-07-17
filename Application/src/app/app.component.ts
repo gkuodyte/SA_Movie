4754470b0b8c86f92590ef44be7e1de37e92de69
Cox <UKX3537@NissanEU.NMCorp.Nissan.Biz>
2018-07-17 15:13:51 +0100

    sdf

--- Application/src/app/app.component.ts
+++ Application/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { MovieService } from "./movies.service";
+import { HttpClient } from "@angular/common/http";
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,13 @@ import { Http, Response } from '@angular/http';
 })
 export class AppComponent {
   title = 'app';
+  storedResponse: any;
+
+  constructor( private movies: MovieService){
+
+  }
+
+  onGetMovieData(){
+    this.storedResponse = this.movies.getData();
+  }
 }
