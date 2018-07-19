b73cd799aaefd0774f127074c5b59a35d24d3e9d
jordanm84 <jordan.marshall@northumbria.ac.uk>
2018-07-19 12:58:06 +0100

    sdf

--- Application/src/app/details/details.component.ts
+++ Application/src/app/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MovieList } from './movie-list.service';
+import { MovieService } from '../movies.service';
 
 @Component({
   selector: 'app-details',
@@ -12,15 +13,19 @@ export class DetailsComponent implements OnInit {
 
   data = this.movieList.getData();
 
-  constructor(private router: Router, private movieList: MovieList) {
+  private selectedMovie;
 
-   }
+  constructor(private router: Router, private movieList: MovieList, private movieService :MovieService) {
+
+    for(let i = 1; i < movieService.movieArray.length; i++){
+      if((movieService.movieArray[i].Title) == this.data){
+        console.log("Found");
+        console.log(movieService.movieArray[i]);
+        this.selectedMovie = movieService.movieArray[i];
+      }
+    }
+  }
 
   ngOnInit() {
   }
-
- 
-    
-
-
 }
